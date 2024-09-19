3e7d5fe87bafaf2a2217c9832caeeeb8f872883e
marco <marcoghidini6@gmail.com>
2024-09-19 13:57:37 +0200

    prova

---
+++ src/HelloDemo.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class HelloDemo {
+
+    private JPanel PannelMain;
+    private JTextField textField1;
+    private JButton button1;
+
+    public JTextField getTextField1() {
+        return textField1;
+    }
+}
