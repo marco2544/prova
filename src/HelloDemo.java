5c567a2f6d68a2561d2037ec76c7d89b332f09bf
marco <marcoghidini6@gmail.com>
2024-09-20 10:15:34 +0200

    rifiniture del test

--- src/HelloDemo.java
+++ src/HelloDemo.java
@@ -1,12 +1,30 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class HelloDemo {
+public class HelloDemo extends JFrame{
 
     private JPanel PannelMain;
     private JTextField textField1;
     private JButton button1;
 
-    public JTextField getTextField1() {
-        return textField1;
+    public HelloDemo() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(button1,textField1.getText());
+                System.out.println(textField1.getText());
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        HelloDemo h = new HelloDemo();
+        h.setContentPane(h.PannelMain);
+        h.setTitle("Hello");
+        h.setSize(300,400);
+        h.setVisible(true);
+        h.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
 }
